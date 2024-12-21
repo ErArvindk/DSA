3a308db29db1f24e09a4a322472113ebe16f285e
ARVIND KUMAR <127136310+ErArvindk@users.noreply.github.com>
2024-12-21 06:18:01 +0000

    container config done

---
+++ Array/createarray.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class createarray {
+
+    public static void main(String[] args) {
+        int Array[]={2,45,67,98};
+        for (int i = 0; i < Array.length; i++) {
+            System.out.print(Array[i]+" ");
+        }
+    }
+}
